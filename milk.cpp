4144d229b6bc4ac49cb77fc969805f5f1a5fea7a
Степнов Никита Дмитриевич <stepnov.2018@stud.nstu.ru>
2021-12-07 12:56:04 +0700

    Decorator

---
+++ milk.cpp
@@ -0,0 +1,16 @@
+#include "milk.h"
+
+Milk::Milk(Beverage *beverage) {
+    this->beverage = beverage;
+}            
+
+char* Milk::getDescription(Beverage*) {
+      char *str = beverage->getDescription();
+      strcpy(str, ", Milk");
+      return str;     
+}
+
+double Milk::cost() {
+       return beverage->cost() + 15.0;
+}
+  
